d8f18657e811f4f1bd974ab43d8d099a6f00e1f7
BogdanATA <batameev@gmail.com>
2026-06-16 17:42:53 -0400

    Added 4 needed entities and their respective repositories

---
+++ src/main/java/com/pluralsight/concerttracker/model/Concert.java
@@ -0,0 +1,94 @@
+package com.pluralsight.concerttracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int concertYear;
+    private double ticketPrice;
+    private int ticketsSold;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "artist_id")
+    private Artist artist;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "promoter_id")
+    private Promoter promoter;
+
+    public Concert() {}
+
+    public Concert(int concertYear, double ticketPrice, int ticketsSold, Artist artist, Venue venue, Promoter promoter) {
+        this.concertYear = concertYear;
+        this.ticketPrice = ticketPrice;
+        this.ticketsSold = ticketsSold;
+        this.artist = artist;
+        this.venue = venue;
+        this.promoter = promoter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getConcertYear() {
+        return concertYear;
+    }
+
+    public void setConcertYear(int concertYear) {
+        this.concertYear = concertYear;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public int getTicketsSold() {
+        return ticketsSold;
+    }
+
+    public void setTicketsSold(int ticketsSold) {
+        this.ticketsSold = ticketsSold;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public Promoter getPromoter() {
+        return promoter;
+    }
+
+    public void setPromoter(Promoter promoter) {
+        this.promoter = promoter;
+    }
+}
